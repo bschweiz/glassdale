1af1a69cf9cd8f779bc957b4e0327e604859efdc
Benjamin Schweizer <bschweiz@gmail.com>
2020-10-18 10:37:40 -0500

    got basic card styling with flex displaying on the page

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import { useCriminals, getCriminals} from "./criminals/CriminalProvider.js"
 import { makeCriminalObj, CriminalCardHTML } from "./criminals/Criminal.js"
-import { CriminalList, insertCriminals } from "./criminals/CriminalList.js"
+import { CriminalList } from "./criminals/CriminalList.js"
 
 
 const bob = makeCriminalObj("Bob Doe",45, "Vehicular Manslaughter","1983-03-07T10:37:22.178Z","2014-03-13T13:57:55.918Z");
@@ -9,7 +9,7 @@ console.table(bob);
 const testHTMLCardToInsert = CriminalCardHTML(bob);
 console.log(testHTMLCardToInsert);
 
-insertCriminals()
+CriminalList()
 
 // console.log(test);
 
