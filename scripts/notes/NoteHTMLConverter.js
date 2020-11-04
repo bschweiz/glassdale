60b798c4ec9e6be3e8fe7b8712ce69634d828a29
Benjamin Schweizer <bschweiz@gmail.com>
2020-11-04 16:15:43 -0600

    just about to start workin on the dropdown

--- scripts/notes/NoteHTMLConverter.js
+++ scripts/notes/NoteHTMLConverter.js
@@ -1,10 +1,8 @@
 export const NoteHTMLCard = (noteObj) => {
     return `
     <div class="note">
-    <h5>Author: ${noteObj.author}</h5>
-    <p>Suspect: ${noteObj.suspect}</p>
-    <p>Date of Interview: ${new Date(noteObj.interviewDate).toDateString()}</p>
-    <p>Time Note Entered: ${new Date(noteObj.timeStamp).toLocaleDateString()}</p>
-    <p>Note: ${noteObj.note}</p>
+    <h5>Note ID: ${noteObj.id}</h5>
+    <p>Suspect: ${noteObj.criminalId}</p>
+    <p>Note: ${noteObj.noteText}</p>
     </div>`
 }
